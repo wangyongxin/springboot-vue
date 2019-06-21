dff0467426344c449fbbd78c0d9687fa568a47ec
wangyongxin <wangyongxin546@163.com>
2019-06-21 12:25:04 +0800

    first commit

---
+++ vue-server/src/main/java/com/wangyx/vueserver/VueSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.wangyx.vueserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueSpringbootApplication.class, args);
+    }
+
+}
